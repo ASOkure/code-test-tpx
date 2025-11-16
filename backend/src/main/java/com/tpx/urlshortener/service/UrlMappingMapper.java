cf8267c0ec8077a193b782d3d0f24320e699281f
akanimo sam okure <akan.okure@gmail.com>
2025-11-16 17:32:36 +0000

    add and implement UrlMappingMapper to convert entiry to UrlResponse DTO

--- backend/src/main/java/com/tpx/urlshortener/service/UrlMappingMapper.java
+++ backend/src/main/java/com/tpx/urlshortener/service/UrlMappingMapper.java
@@ -1,2 +1,16 @@
-package com.tpx.urlshortener.service;public class UrlMappingMapper {
+package com.tpx.urlshortener.service;
+import com.tpx.urlshortener.dto.UrlResponse;
+import com.tpx.urlshortener.entity.UrlMapping;
+
+public class UrlMappingMapper {
+
+    private static final String BASE_URL = "http://localhost:8080";
+
+    public static UrlResponse toResponse(UrlMapping mapping) {
+        return UrlResponse.builder()
+                .alias(mapping.getAlias())
+                .fullUrl(mapping.getFullUrl())
+                .shortUrl(BASE_URL + "/" + mapping.getAlias())
+                .build();
+    }
 }
